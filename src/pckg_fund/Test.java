ffeae4fc6fbcc8f1e4ef405133076681f08f5144
Student <kholovka21@student.unizd.hr>
2022-03-31 17:57:46 +0200

    init commit for ALgoritmi i strukture podataka

---
+++ src/pckg_fund/Test.java
@@ -0,0 +1,16 @@
+package pckg_fund;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int[] arr = {11, -134, 98, 56, 78, 0, 34};
+        int[] arrRemov = AUX_ALG.removeElementFromArrayPos(arr, 3);
+        System.out.println("=============================");
+        //System.out.println(Arrays.toString(arrAdded));
+
+
+
+    }
+}
